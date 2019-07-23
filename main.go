cdf5305ca1cd36e1df0c216583d9fdc5a9189f12
rexllz <lzliurex@gmail.com>
2019-07-23 18:29:29 +0800

    get the parameters from the frontend

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"io"
 	"net/http"
 )
 
@@ -10,11 +9,33 @@ func main() {
 	//bind the func and request
 	http.HandleFunc("/user/login",
 		func(writer http.ResponseWriter, request *http.Request) {
-		io.WriteString(writer,"hello world")
+
+			request.ParseForm()
+			mobile := request.PostForm.Get("mobile")
+			passwd := request.PostForm.Get("passwd")
+			loginok := false
+
+			if (mobile == "186" && passwd == "123") {
+
+				loginok = true
+			}
+
+			str := `{"code":0,"data":{"id":1,"token":"test"}}`
+
+			if !loginok {
+
+				str = `{"code":-1,"msg":"password wrong"}`
+			}
+			//set header
+			writer.Header().Set("Content-Type","application/json")
+			writer.WriteHeader(http.StatusOK)
+
+			writer.Write([]byte(str))
+
+			//io.WriteString(writer,"hello world")
 	})
 
 	//start the web server
 	http.ListenAndServe(":8080",nil)
-
 
 }
